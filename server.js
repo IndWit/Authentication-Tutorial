2e8d817745e48a417286c05540a7519cf6f83858
Induma Withanage <164716763+IndWit@users.noreply.github.com>
2025-11-18 23:33:40 +0530

    Add initial project dependencies and .env file
    Added a .env file and installed various Node.js dependencies, including Express, dotenv, jsonwebtoken, and supporting libraries. This sets up the foundational environment and packages required for the project.

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username:'Kyle',
+        title: 'post 1'
+    },
+    {
+        username:'Jim',
+        title: 'post 2'
+    }
+
+]
+
+
+app.get('/posts', (req, res) => {
+    res.json(posts)
+})
+
+app.post('/login', (req, res) => {
+    //Authentication
+    const username = req.body.username
+    const user =  {name: username}
+
+    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken:accessToken})
+})
+
+app.listen(3000)
